94420e469b6b1842c70474992d655a6b7b50ef0a
jinuk <wooyoomisutgaru@gmail.com>
2016-08-29 14:48:12 +0900

    aa

--- src/main/java/com/sting/dao/UserDao.java
+++ src/main/java/com/sting/dao/UserDao.java
@@ -1,12 +1,15 @@
 package com.sting.dao;
 
 import com.sting.vo.UserVo;
+
+import java.util.HashMap;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserDao {
 	
 	public UserVo getUserById(String id);
-	public void makeuser(String id);
+	public void makeuser(HashMap<String,String> map);
 	public int checkid(String id);
 }
---
+++ src/main/java/com/sting/vo/CompanyVo.java
@@ -0,0 +1,61 @@
+package com.sting.vo;
+
+public class CompanyVo {
+	private int cid;
+	private int uid;
+	private String registration_number;
+	private String manager_name;
+	private String manager_phonenumber;
+	private String interestproduct;
+	private String typeofcompany;
+	private String company_name;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getRegistration_number() {
+		return registration_number;
+	}
+	public void setRegistration_number(String registration_number) {
+		this.registration_number = registration_number;
+	}
+	public String getManager_name() {
+		return manager_name;
+	}
+	public void setManager_name(String manager_name) {
+		this.manager_name = manager_name;
+	}
+	public String getManager_phonenumber() {
+		return manager_phonenumber;
+	}
+	public void setManager_phonenumber(String manager_phonenumber) {
+		this.manager_phonenumber = manager_phonenumber;
+	}
+	public String getInterestproduct() {
+		return interestproduct;
+	}
+	public void setInterestproduct(String interestproduct) {
+		this.interestproduct = interestproduct;
+	}
+	public String getTypeofcompany() {
+		return typeofcompany;
+	}
+	public void setTypeofcompany(String typeofcompany) {
+		this.typeofcompany = typeofcompany;
+	}
+	public String getCompany_name() {
+		return company_name;
+	}
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+	
+}
