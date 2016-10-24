7d2dd25094c597a3c8e1afd9fb713ce6b77a69cc
heedo <limhd6958@gmail.com>
2016-10-24 14:33:42 +0900

    qwe

--- src/main/java/com/sting/controller/AdminController.java
+++ src/main/java/com/sting/controller/AdminController.java
@@ -4,39 +4,27 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.sting.dao.NoticeDao;
 import com.sting.dao.QnaDao;
 import com.sting.dao.UserDao;
-import com.sting.vo.NoticeVo;
 import com.sting.vo.QnaVo;
 import com.sting.vo.UserVo;
-
-import org.springframework.ui.Model;
-import org.springframework.stereotype.Controller;
 
 
 @Controller
